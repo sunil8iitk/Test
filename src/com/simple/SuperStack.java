1e7efd71cb2e5af9e53eb5172007e10b1e8f1165
sunmocha <sunmocha@in.ibm.com>
2017-06-29 09:19:44 +0530

    29-Jun-17 : Going to build a super stack.

---
+++ src/com/simple/SuperStack.java
@@ -0,0 +1,9 @@
+package com.simple;
+
+public class SuperStack 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Going to build a stack.");
+	}
+}
--- src/com/test/TestPolymorphism3.java
+++ src/com/test/TestPolymorphism3.java
@@ -1,26 +1,37 @@
 package com.test;
 
+class TestPolymorphism3 {
+	public static void main(String[] args) {
+		Animal a;
+		a = new Dog();
+		a.eat();
+		a = new Cat();
+		a.eat();
+		a = new Lion();
+		a.eat();
+	}
+}
 
-class TestPolymorphism3{  
-public static void main(String[] args){  
-Animal a;  
-a=new Dog();  
-a.eat();  
-a=new Cat();  
-a.eat();  
-a=new Lion();  
-a.eat();  
-}}  
+class Animal {
+	void eat() {
+		System.out.println("eating...");
+	}
+}
 
-class Animal{  
-void eat(){System.out.println("eating...");}  
-}  
-class Dog extends Animal{  
-void eat(){System.out.println("eating bread...");}  
-}  
-class Cat extends Animal{  
-void eat(){System.out.println("eating rat...");}  
-}  
-class Lion extends Animal{  
-void eat(){System.out.println("eating meat...");}  
-}  
+class Dog extends Animal {
+	void eat() {
+		System.out.println("eating bread...");
+	}
+}
+
+class Cat extends Animal {
+	void eat() {
+		System.out.println("eating rat...");
+	}
+}
+
+class Lion extends Animal {
+	void eat() {
+		System.out.println("eating meat...");
+	}
+}
